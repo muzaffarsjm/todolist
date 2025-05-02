daca27154d4b937cb420f8eaee6feca4669cae1e
muzaffar <smuzaffarov.1996@gmail.com>
2025-05-02 13:00:24 +0900

    nuances

---
+++ todolist.js
@@ -0,0 +1,47 @@
+let todoList = [{
+  name: 'Check email',
+  dueDate: '2025-03-6'
+}];
+
+
+render();
+
+function render(){
+  let todoListHTML = '';
+
+  for (let i = 0; i< todoList.length; i++){
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div> 
+      <button onclick ="todoList.splice(${i}, 1);
+      render();"
+      class = "delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+
+  }
+
+  document.querySelector('.todo-container').innerHTML = todoListHTML;
+}
+
+function addTodo(){
+  let inputElement = document.querySelector('.js-input');
+  let name = inputElement.value;
+
+  const dateInput = document.querySelector('.js-date');
+  const dueDate = dateInput.value;
+
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate
+    name,
+    dueDate
+  });
+  console.log(todoList)
+  inputElement.value = '';
+  render(); 
+};
